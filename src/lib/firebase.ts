5423f36cd698c5fd8883cea3079b50b1ad8fa666
Jed Geoghegan <jedgeoghegan@gmail.com>
2025-03-05 20:15:29 -0500

    kept only needed firebase credentials

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,6 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getDatabase, ref, increment, set, onValue, push, serverTimestamp } from 'firebase/database';
 
 const firebaseConfig = {
-  // TODO: Replace with your Firebase config
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
 };
 
